9830220e5d624f39cc760b0b200d5715471eca8e
Bruno <brn.milano@hotmail.com>
2022-02-27 00:07:45 -0300

    Ajustes no componente Apresentação

---
+++ src/App/Pages/Home/Apresentacao/index.tsx
@@ -0,0 +1,97 @@
+import Box from "Components/Box";
+import Container from "Components/Container";
+import FadeInWhenVisible from "Components/Animations/FadeInWhenVisible";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Heading from "Components/Heading";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import styles from "./styles.module.scss";
+import Text from "Components/Text";
+
+export default function Professor() {
+  return (
+    <Box className={styles.container}>
+      <Box className={styles.content}>
+        <FadeInWhenVisible>
+          <Box className={styles.card}>
+            <Box className={styles.cardImage}>
+              <Box className={styles.imageContainer}>
+                <img alt="Bruno Milano" src="/images/bruno-milano.png" />
+              </Box>
+            </Box>
+
+            <Box marginTop={90}>
+              <Heading
+                className={styles.cardTitle}
+                fontFamily="Cabin"
+                weight="semi-bold"
+              >
+                Bruno Milano
+              </Heading>
+            </Box>
+
+            <Heading className={styles.cardSubtitle} size={20}>
+              Desenvolvedor a 1 ano
+            </Heading>
+
+            <Box className={styles.textContainer}>
+              <Text color="white">
+                Ao me ver rodeado de programadores, decidi entrar de cabeça no
+                mundo da tecnologia. Com muito foco tive uma evolução
+                extraordinária e hoje faço parte de uma escola para novos devs
+                front-end, além de atuar diretamente em diversos projetos de
+                grandes empresas brasileiras, inclusive empresas internacionais.
+              </Text>
+            </Box>
+
+            <Box className={styles.contact}>
+              <Box display="flex" alignItems="center" gap={10}>
+                <img alt="Localiação" src="/images/localizacao.png" />
+                <Text color="lightGray">Brasília, DF - Brasil</Text>
+              </Box>
+
+              <Box display="flex" alignItems="center" gap={10}>
+                <img
+                  className={styles.email}
+                  alt="E-mail"
+                  src="/images/email.png"
+                />
+                <Text color="lightGray">brnmilano.dev@gmail.com</Text>
+              </Box>
+
+              <Box display="flex" alignItems="center" gap={9}>
+                <img
+                  className={styles.telefone}
+                  alt="Telefone"
+                  src="/images/telefone.png"
+                />
+                <Text color="lightGray">(61) 9 9882-2870 / (61) 9 8257-5381</Text>
+              </Box>
+            </Box>
+
+            <Box alignItems="center" display="flex" marginTop={40}>
+              <a
+                className={styles.socialMedia}
+                href="https://github.com/brunolucena"
+                id="github-bruno-lucena"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <GitHubIcon style={{ color: "#5C6BC0", fontSize: 34 }} />
+              </a>
+
+              <a
+                className={styles.socialMedia}
+                href="https://www.linkedin.com/in/bruno-lucena"
+                id="linkedin-bruno-lucena"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <LinkedInIcon style={{ color: "#0076FF", fontSize: 34 }} />
+              </a>
+            </Box>
+          </Box>
+        </FadeInWhenVisible>
+      </Box>
+    </Box>
+  );
+}
