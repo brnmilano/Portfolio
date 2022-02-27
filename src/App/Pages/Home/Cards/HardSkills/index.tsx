af44bf0a9896c131ef48c9d55e5f08da47e4f1c0
Bruno <brn.milano@hotmail.com>
2022-02-27 01:25:05 -0300

    Ajustes no componente HardSkills

--- src/App/Pages/Home/Cards/HardSkills/index.tsx
+++ src/App/Pages/Home/Cards/HardSkills/index.tsx
@@ -6,79 +6,81 @@ import FadeInWhenVisible from "Components/Animations/FadeInWhenVisible";
 export default function HardSkills() {
   return (
     <Box className={styles.container}>
-      <FadeInWhenVisible>
-        <Box className={styles.card}>
-          <Heading color="white">Tecnologias e frameworks</Heading>
+      <Box className={styles.content}>
+        <FadeInWhenVisible>
+          <Box className={styles.card}>
+            <Heading color="white">Tecnologias e frameworks</Heading>
 
-          <Box className={styles.lineShadow}></Box>
+            <Box className={styles.lineShadow}></Box>
 
-          <Box className={styles.skills}>
-            <Box>
-              <img
-                className={styles.imageSkills}
-                src="images/html.png"
-                alt="HTML 5"
-              />
-            </Box>
+            <Box className={styles.skills}>
+              <Box>
+                <img
+                  className={styles.imageSkills}
+                  src="images/html.png"
+                  alt="HTML 5"
+                />
+              </Box>
 
-            <Box>
-              <img
-                className={styles.imageSkills}
-                src="images/css.png"
-                alt="CSS 3"
-              />
-            </Box>
+              <Box>
+                <img
+                  className={styles.imageSkills}
+                  src="images/css.png"
+                  alt="CSS 3"
+                />
+              </Box>
 
-            <Box>
-              <img
-                className={styles.imageSkills}
-                src="images/java-script.png"
-                alt="JavaScript"
-              />
-            </Box>
+              <Box>
+                <img
+                  className={styles.imageSkills}
+                  src="images/java-script.png"
+                  alt="JavaScript"
+                />
+              </Box>
 
-            <Box>
-              <img
-                className={styles.imageSkills}
-                src="images/type-script.png"
-                alt="TypeScript"
-              />
-            </Box>
+              <Box>
+                <img
+                  className={styles.imageSkills}
+                  src="images/type-script.png"
+                  alt="TypeScript"
+                />
+              </Box>
 
-            <Box>
-              <img
-                className={styles.imageSkills}
-                src="images/react.png"
-                alt="React.JS"
-              />
-            </Box>
+              <Box>
+                <img
+                  className={styles.imageSkills}
+                  src="images/react.png"
+                  alt="React.JS"
+                />
+              </Box>
 
-            <Box>
-              <img
-                className={styles.imageSkills}
-                src="images/next-js.png"
-                alt="Next.JS"
-              />
-            </Box>
+              <Box>
+                <img
+                  className={styles.imageSkills}
+                  src="images/next-js.png"
+                  alt="Next.JS"
+                />
+              </Box>
 
-            <Box>
-              <img
-                className={styles.imageSkills}
-                src="images/git.png"
-                alt="Git"
-              />
-            </Box>
+              <Box>
+                <img
+                  className={styles.imageSkills}
+                  src="images/git.png"
+                  alt="Git"
+                />
+              </Box>
 
-            <Box>
-              <img
-                className={styles.imageSkills}
-                src="images/design-responsivo.png"
-                alt="Layouts Responsivos"
-              />
+              <Box>
+                <img
+                  className={styles.imageSkills}
+                  src="images/design-responsivo.png"
+                  alt="Layouts Responsivos"
+                />
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </FadeInWhenVisible>
+        </FadeInWhenVisible>
+      </Box>
     </Box>
   );
 }
