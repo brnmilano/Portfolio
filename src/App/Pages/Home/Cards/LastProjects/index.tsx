73cc69272a7e6dd8ba03cac1da36603c9c8692d2
Bruno <brn.milano@hotmail.com>
2022-02-27 14:09:16 -0300

    Ajustes no componente LastProjects

--- src/App/Pages/Home/Cards/LastProjects/index.tsx
+++ src/App/Pages/Home/Cards/LastProjects/index.tsx
@@ -2,7 +2,9 @@ import Box from "Components/Box";
 import Heading from "Components/Heading";
 import styles from "./styles.module.scss";
 import Text from "Components/Text";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Link from "next/link";
 
 export default function LastProjects() {
   return (
@@ -11,10 +13,89 @@ export default function LastProjects() {
         <Box className={styles.card}>
           <Box display="flex" justifyContent="between" alignItems="center">
             <Heading color="white">Últimos projetos</Heading>
-            <Button classes={{ root: styles.button }} href="#text-buttons">Ver todos</Button>
+            <Button classes={{ root: styles.button }} href="#text-buttons">
+              Ver todos
+            </Button>
           </Box>
 
           <Box className={styles.lineShadow}></Box>
+
+          <Box className={styles.projects}>
+            <Box className={styles.projectCard}>
+              <img
+                className={styles.projectsImages}
+                src="/images/mr-green.png"
+                alt=""
+              />
+              <Text color="white">Mr. Green, projeto feito em Next.JS</Text>
+
+              <Box className={styles.arrow}>
+                <Button classes={{ root: styles.button }} href="#text-buttons">
+                  <ArrowForwardIosIcon />
+                </Button>
+              </Box>
+            </Box>
+
+            <Box className={styles.projectCard}>
+              <img
+                className={styles.projectsImages}
+                src="/images/front-academy.png"
+                alt=""
+              />
+              <Text color="white">Mr. Green, projeto feito em Next.JS</Text>
+
+              <Box className={styles.arrow}>
+                <Button classes={{ root: styles.button }} href="#text-buttons">
+                  <ArrowForwardIosIcon />
+                </Button>
+              </Box>
+            </Box>
+
+            <Box className={styles.projectCard}>
+              <img
+                className={styles.projectsImages}
+                src="/images/decathlon.png"
+                alt=""
+              />
+              <Text color="white">Mr. Green, projeto feito em Next.JS</Text>
+
+              <Box className={styles.arrow}>
+                <Button classes={{ root: styles.button }} href="#text-buttons">
+                  <ArrowForwardIosIcon />
+                </Button>
+              </Box>
+            </Box>
+
+            <Box className={styles.projectCard}>
+              <img
+                className={styles.projectsImages}
+                src="/images/cantinho.png"
+                alt=""
+              />
+              <Text color="white">Mr. Green, projeto feito em Next.JS</Text>
+
+              <Box className={styles.arrow}>
+                <Button classes={{ root: styles.button }} href="#text-buttons">
+                  <ArrowForwardIosIcon />
+                </Button>
+              </Box>
+            </Box>
+
+            <Box className={styles.projectCard}>
+              <img
+                className={styles.projectsImages}
+                src="/images/depoimentos.png"
+                alt=""
+              />
+              <Text color="white">Mr. Green, projeto feito em Next.JS</Text>
+
+              <Box className={styles.arrow}>
+                <Button classes={{ root: styles.button }} href="#text-buttons">
+                  <ArrowForwardIosIcon />
+                </Button>
+              </Box>
+            </Box>
+          </Box>
         </Box>
       </Box>
     </Box>
