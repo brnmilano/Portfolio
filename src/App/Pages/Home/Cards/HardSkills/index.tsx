a508ae205983f021842ca20c0748a6ac6f1cff35
Bruno <brn.milano@hotmail.com>
2022-02-27 01:16:05 -0300

    Add skill images e ajustes no componente HardSkills

--- src/App/Pages/Home/Cards/HardSkills/index.tsx
+++ src/App/Pages/Home/Cards/HardSkills/index.tsx
@@ -1,7 +1,7 @@
-import Box from 'Components/Box';
-import Heading from 'Components/Heading';
-import styles from './styles.module.scss';
-import Text from 'Components/Text';
+import Box from "Components/Box";
+import Heading from "Components/Heading";
+import styles from "./styles.module.scss";
+import Text from "Components/Text";
 
 export default function HardSkills() {
   return (
@@ -10,6 +10,40 @@ export default function HardSkills() {
         <Heading color="white">Tecnologias e frameworks</Heading>
 
         <Box className={styles.lineShadow}></Box>
+
+        <Box className={styles.skills}>
+          <Box>
+            <img className={styles.imageSkills} src="images/html.png" alt="HTML 5" />
+          </Box>
+
+          <Box>
+            <img className={styles.imageSkills} src="images/css.png" alt="CSS 3" />
+          </Box>
+
+          <Box>
+            <img className={styles.imageSkills} src="images/java-script.png" alt="JavaScript" />
+          </Box>
+
+          <Box>
+            <img className={styles.imageSkills} src="images/type-script.png" alt="TypeScript" />
+          </Box>
+
+          <Box>
+            <img className={styles.imageSkills} src="images/react.png" alt="React.JS" />
+          </Box>
+
+          <Box>
+            <img className={styles.imageSkills} src="images/next-js.png" alt="Next.JS" />
+          </Box>
+
+          <Box>
+            <img className={styles.imageSkills} src="images/git.png" alt="Git" />
+          </Box>
+
+          <Box>
+            <img className={styles.imageSkills} src="images/design-responsivo.png" alt="Layouts Responsivos" />
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
