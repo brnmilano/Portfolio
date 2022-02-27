d3b9c2ba4d97980c815daa86909a9ad2ed5565af
Bruno <brn.milano@hotmail.com>
2022-02-27 00:11:48 -0300

    Remoção de imagens e arquivos não utilizáveis

--- src/App/Pages/Home/index.tsx
+++ src/App/Pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import Box from "Components/Box";
 import styles from "./styles.module.scss";
-import Apresentação from "./Apresentação";
+import Apresentação from "./Apresentacao";
 import Cards from "./Cards";
 
 export default function Home() {
