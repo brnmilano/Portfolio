13c59938fdd7822cc84b4f34ac51b08fcc3f43de
Bruno <brn.milano@hotmail.com>
2022-02-27 01:43:10 -0300

    Ajustes nos componentes LastProjects e Certificados

--- src/App/Pages/Home/Cards/Certificados/index.tsx
+++ src/App/Pages/Home/Cards/Certificados/index.tsx
@@ -1,13 +1,23 @@
-import Box from 'Components/Box';
-import Heading from 'Components/Heading';
-import styles from './styles.module.scss';
-import Text from 'Components/Text';
+import Box from "Components/Box";
+import Heading from "Components/Heading";
+import styles from "./styles.module.scss";
+import Text from "Components/Text";
+import Button from "@mui/material/Button";
 
 export default function Certificados() {
   return (
     <Box className={styles.container}>
-      <Box className={styles.card}>
-        <Heading color="white">Certificados</Heading>
+      <Box className={styles.content}>
+        <Box className={styles.card}>
+          <Box display="flex" justifyContent="between" alignItems="center">
+            <Heading color="white">Certificados</Heading>
+            <Button classes={{ root: styles.button }} href="#text-buttons">
+              Ver todos
+            </Button>
+          </Box>
+
+          <Box className={styles.lineShadow}></Box>
+        </Box>
       </Box>
     </Box>
   );
--- src/App/Pages/Home/Cards/LastProjects/index.tsx
+++ src/App/Pages/Home/Cards/LastProjects/index.tsx
@@ -1,13 +1,21 @@
-import Box from 'Components/Box';
-import Heading from 'Components/Heading';
-import styles from './styles.module.scss';
-import Text from 'Components/Text';
+import Box from "Components/Box";
+import Heading from "Components/Heading";
+import styles from "./styles.module.scss";
+import Text from "Components/Text";
+import Button from '@mui/material/Button';
 
 export default function LastProjects() {
   return (
     <Box className={styles.container}>
-      <Box className={styles.card}>
-        <Heading color="white">Últimos projetos</Heading>
+      <Box className={styles.content}>
+        <Box className={styles.card}>
+          <Box display="flex" justifyContent="between" alignItems="center">
+            <Heading color="white">Últimos projetos</Heading>
+            <Button classes={{ root: styles.button }} href="#text-buttons">Ver todos</Button>
+          </Box>
+
+          <Box className={styles.lineShadow}></Box>
+        </Box>
       </Box>
     </Box>
   );
