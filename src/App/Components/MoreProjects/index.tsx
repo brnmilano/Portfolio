685fc957a64c518ab459dcfa31bfbaf8d2cc3b2d
Bruno <brn.milano@hotmail.com>
2022-02-28 21:55:25 -0300

    Adição de novos componentes + organização das imagens

--- src/App/Components/MoreProjects/index.tsx
+++ src/App/Components/MoreProjects/index.tsx
@@ -1,12 +1,13 @@
-import Box from 'Components/Box';
-import Cantinho from './Cantinho';
-import DmRoldanas from './Dm';
-import FrontAcademy from './FrontAcademy';
-import Heading from 'Components/Heading';
-import Loki from './Depoimentos';
-import MrGreen from './MrGreen';
-import styles from './styles.module.scss';
-import TimeControl from './TimeControl';
+import Box from "Components/Box";
+import Cantinho from "./Cantinho";
+import DmRoldanas from "./Dm";
+import FrontAcademy from "./FrontAcademy";
+import Heading from "Components/Heading";
+import Loki from "./Depoimentos";
+import MrGreen from "./MrGreen";
+import styles from "./styles.module.scss";
+import TimeControl from "./TimeControl";
+import Login from "./Login";
 
 export default function MoreProjects() {
   return (
@@ -18,7 +19,7 @@ export default function MoreProjects() {
         </Heading>
       </Box>
 
-      <Box display="flex" gap={20}>
+      <Box display="flex" alignItems="center" gap={20}>
         <MrGreen />
         <FrontAcademy />
       </Box>
@@ -30,6 +31,11 @@ export default function MoreProjects() {
 
       <Box display="flex" gap={20}>
         <TimeControl />
+        <Loki />
+      </Box>
+
+      <Box display="flex" gap={20}>
+        <Login />
         <Loki />
       </Box>
     </Box>
