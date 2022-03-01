69d76bf34b9c409279456fddfd91c1c46c66ba47
Bruno <brn.milano@hotmail.com>
2022-02-28 21:14:18 -0300

    Fix

--- src/App/Components/MoreProjects/index.tsx
+++ src/App/Components/MoreProjects/index.tsx
@@ -1,5 +1,7 @@
 import Box from "Components/Box";
 import Heading from "Components/Heading";
+import Cantinho from "./Cantinho";
+import DmRoldanas from "./Dm";
 import FrontAcademy from "./FrontAcademy";
 import MrGreen from "./MrGreen";
 import styles from "./styles.module.scss";
@@ -7,15 +9,21 @@ import styles from "./styles.module.scss";
 export default function MoreProjects() {
   return (
     <Box className={styles.container}>
-      <Box>
+      <Box className={styles.title}>
         <Heading color="white">
-          Aqui você irá encontrar todos os meus projetos!
+          Aqui você irá encontrar todos os projetos que já fiz e já participei
+          durante minha carreira como dev front-end!
         </Heading>
       </Box>
 
       <Box display="flex" gap={20}>
         <MrGreen />
         <FrontAcademy />
+      </Box>
+
+      <Box display="flex" gap={20}>
+        <Cantinho />
+        <DmRoldanas />
       </Box>
     </Box>
   );
