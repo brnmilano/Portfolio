c5399e99a0cf6315371639fd0ac68fd84bbc06c9
Bruno <brn.milano@hotmail.com>
2022-03-01 19:22:10 -0300

    Criação do componente Alura

---
+++ src/App/Components/MoreCertificates/Alura/index.tsx
@@ -0,0 +1,39 @@
+import Box from "Components/Box";
+import styles from "./styles.module.scss";
+import Text from "Components/Text";
+import { Button } from "@mui/material";
+import Link from "next/link";
+
+export default function Alura() {
+  return (
+    <Box className={styles.certified}>
+      <Box>
+        <img
+          className={styles.certifiedImages}
+          src="/images/alura.png"
+          alt=""
+        />
+
+        <Button classes={{ root: styles.button }}>
+          <Link href="https://imgur.com/i3zTnT1">
+            <a className={styles.link} target="_blank">
+              <Box className={styles.certifiedCard}>
+                <Text weight="bold" color="white">
+                  Formação Front-End
+                </Text>
+
+                <Text weight="bold" color="white">
+                  Alura, 2021
+                </Text>
+
+                <Text italic size={12} color="white">
+                  (HTML, CSS, JavaScript, React e Layouts Responsivos)
+                </Text>
+              </Box>
+            </a>
+          </Link>
+        </Button>
+      </Box>
+    </Box>
+  );
+}
--- src/App/Components/MoreCertificates/index.tsx
+++ src/App/Components/MoreCertificates/index.tsx
@@ -1,15 +1,22 @@
-import Box from 'Components/Box';
-import Cis from './Cis';
-import Heading from 'Components/Heading';
-import styles from './styles.module.scss';
+import Box from "Components/Box";
+import Cis from "./Cis";
+import Heading from "Components/Heading";
+import styles from "./styles.module.scss";
+import Alura from "./Alura";
 
 export default function MoreCertificates() {
-  return(
+  return (
     <Box className={styles.container}>
       <Box>
-        <Heading color="white">Aqui você irá encontrar todos os meus certificados!</Heading>
+        <Heading color="white">
+          Aqui você irá encontrar todos os meus certificados!
+        </Heading>
       </Box>
-      <Cis />
+
+      <Box display="flex" alignItems="center" gap={20}>
+        <Cis />
+        <Alura />
+      </Box>
     </Box>
   );
 }
